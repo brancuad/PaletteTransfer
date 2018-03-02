af4a448c3e6d40267e1646d7e7d671e0f5deb3a0
Brandon Cuadrado <brancuad@gmail.com>
2018-03-01 19:51:02 -0500

    Load Image and get pixel data in JavaScript

--- PaletteProject/scripts/loadimage.js
+++ PaletteProject/scripts/loadimage.js
@@ -1,19 +1,58 @@
 // Loading an image
 
-window.onload = function () {
-	var canvas = document.getElementById('origin');
-	var context = canvas.getContext('2d');
+var sourceImg = "../images/test.png"
+
+// origin image struct
+var origin = {
+	img: new Image(),
+
+	// draw image in the canvas. parameters are the offset
+	drawImage: function (x = 0, y = 0) {
+		var maxWidth = $("#origin").width();
+		var maxHeight = $("#origin").height();
+
+		var ratio = Math.min(maxWidth / this.img.width,
+			maxHeight / this.img.height);
+		this.context.drawImage(this.img, x, y,
+			this.img.width * ratio, this.img.height * ratio);
+	},
+
+	getImageData: function (x = 0, y = 0) {
+		flatData = this.context.getImageData(x, y, this.canvas.width(), this.canvas.height());
+
+		data = []
+		for (var i = 0; i < flatData.data.length; i += 4) {
+			pixel = {}
+			pixel.r = flatData.data[i];
+			pixel.g = flatData.data[i + 1];
+			pixel.b = flatData.data[i + 2];
+			pixel.a = flatData.data[i + 3];
+
+			data.push(pixel);
+		}
+
+		return data;
+
+	}
+}
+
+$(document).ready(function () {
+	origin.canvas = $("#origin");
+	origin.context = origin.canvas[0].getContext('2d');
 
 	make_base();
 
 	function make_base() {
-		base_image = new Image(300, 150);
-		base_image.crossOrigin = "Anonymous";
-		base_image.onload = function () {
-			context.drawImage(base_image, 0, 0, base_image.width, base_image.height);
-			data = context.getImageData(10, 10, 1, 1);
-		}
-		base_image.src = 'C:/Users/branc/Pictures/Mine/yellow.jpg';
-	}
 
-};
+		origin.img.onload = function () {
+			// draw image in canvas
+			origin.drawImage();
+
+			// get image array
+			data = origin.getImageData();
+			pixel = data[0];
+		}
+
+		origin.img.src = sourceImg;
+	}
+});
