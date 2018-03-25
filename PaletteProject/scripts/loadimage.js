716ff3d9876c5dcefdf172bb855562d1c9246e76
Brandon Cuadrado <brancuad@gmail.com>
2018-03-25 17:25:03 -0400

    Calculate palette using histogram bins

--- PaletteProject/scripts/loadimage.js
+++ PaletteProject/scripts/loadimage.js
@@ -51,11 +51,186 @@ var origin = {
 
 	},
 
-	getPalette: function () {
-		if (!this.pixels)
-			return;
+	// Plot histogram of bins
+	// Return means of each bin in LAB color space
+	getKmeansData: function () {
+		// Get histogram of all the colors
+		// Derived from https://billmill.org/the_histogram.html
+		var colorArray = function () {
+			arr = [];
+			for (var i = 0; i < 16; i++) {
+				arr[i] = []
+			}
+			return arr;
+		};
 
-		return clusterfck.kmeans(this.pixels, 5);
+		var rbins = colorArray();
+		var gbins = colorArray();
+		var bbins = colorArray();
+
+		// Separate pixels into bins for each color channel
+		for (var i = 0; i < this.pixels.length; i++) {
+			var getBin = function (num) {
+				return Math.floor(num / 16);
+			}
+			pixel = this.pixels[i];
+			rbins[getBin(pixel[0])].push(pixel);
+			gbins[getBin(pixel[1])].push(pixel);
+			bbins[getBin(pixel[2])].push(pixel);
+		}
+
+		// Display histograms
+		var histCtx = $("#originHist")[0].getContext('2d');
+
+		var maxBinSize = function (bins) {
+			var maxLength = 0;
+			for (var i = 0; i < bins.length; i++) {
+				var bin = bins[i];
+
+				if (bin.length > maxLength) {
+					maxLength = bin.length;
+				}
+			}
+
+			return maxLength;
+		}
+
+		var rmax = maxBinSize(rbins);
+		var gmax = maxBinSize(gbins);
+		var bmax = maxBinSize(bbins);
+
+		var colorbars = function (max, bins, color, y) {
+			histCtx.fillStyle = color;
+			jQuery.each(bins, function (i, x) {
+				var pct = (bins[i].length / max) * 130;
+				histCtx.fillRect(i * 16, y, 15, -Math.round(pct));
+			});
+		};
+
+		colorbars(rmax, rbins, "rgb(255,0,0)", 133);
+		colorbars(gmax, gbins, "rgb(0,255,0)", 266);
+		colorbars(bmax, bbins, "rgb(0,0,255)", 399);
+
+		// Get mean color per bin in lab color space
+		var rmeans = colorArray();
+		var gmeans = colorArray();
+		var bmeans = colorArray();
+
+		// Convert to lab and average all bins
+		for (var i = 0; i < rbins.length; i++) {
+			rbin = rbins[i];
+			gbin = gbins[i];
+			bbin = bbins[i];
+
+			rlabBin = []
+			glabBin = []
+			blabBin = []
+
+			if (rbin.length > 0) {
+				// Convert rgb pixels to lab
+				for (var j = 0; j < rbin.length; j++) {
+					var rlab = rgb2lab(rbin[j]);
+					rlabBin[j] = rlab;
+				}
+
+				// Average rlab values
+				lTotal = 0;
+				aTotal = 0;
+				bTotal = 0;
+				for (var j = 0; j < rlabBin.length; j++) {
+					var labPixel = rlabBin[j];
+					lTotal += labPixel[0];
+					aTotal += labPixel[1];
+					bTotal += labPixel[2];
+				}
+				rmeans[i].push(lTotal / rlabBin.length);
+				rmeans[i].push(aTotal / rlabBin.length);
+				rmeans[i].push(bTotal / rlabBin.length);
+			}
+
+			if (gbin.length > 0) {
+				// Convert and average green bin
+				for (var j = 0; j < gbin.length; j++) {
+					var glab = rgb2lab(gbin[j]);
+					glabBin[j] = glab;
+				}
+
+				lTotal = 0;
+				aTotal = 0;
+				bTotal = 0;
+				for (var j = 0; j < glabBin.length; j++) {
+					var labPixel = glabBin[j];
+					lTotal += labPixel[0];
+					aTotal += labPixel[1];
+					bTotal += labPixel[2];
+				}
+				gmeans[i].push(lTotal / glabBin.length);
+				gmeans[i].push(aTotal / glabBin.length);
+				gmeans[i].push(bTotal / glabBin.length);
+
+
+			}
+
+			if (bbin.length > 0) {
+				// Convert and average blue bin
+				for (var j = 0; j < bbin.length; j++) {
+					var blab = rgb2lab(bbin[j]);
+					blabBin[j] = blab;
+				}
+
+				lTotal = 0;
+				aTotal = 0;
+				bTotal = 0;
+				for (var j = 0; j < blabBin.length; j++) {
+					var labPixel = blabBin[j];
+					lTotal += labPixel[0];
+					aTotal += labPixel[1];
+					bTotal += labPixel[2];
+				}
+				bmeans[i].push(lTotal / blabBin.length);
+				bmeans[i].push(aTotal / blabBin.length);
+				bmeans[i].push(bTotal / blabBin.length);
+
+			}
+
+		}
+
+		var removeEmpty = function (meansArray) {
+			var arr = [];
+
+			for (var i = 0; i < meansArray.length; i++) {
+				if (meansArray[i].length == 0)
+					continue;
+				arr.push(meansArray[i]);
+			}
+
+			return arr;
+		}
+
+		var means = [];
+
+		means = means.concat(removeEmpty(rmeans));
+		means = means.concat(removeEmpty(gmeans));
+		means = means.concat(removeEmpty(bmeans));
+
+		return means;
+	},
+
+	getPalette: function () {
+
+		var kmeansPoints = origin.getKmeansData();
+
+		var kMeansResult = clusterfck.kmeans(kmeansPoints, 5);
+
+		var palette = []
+		for (var i = 0; i < kMeansResult.centroids.length; i++) {
+			palette.push(lab2rgb(kMeansResult.centroids[i]));
+		}
+
+		return {
+			clusters: kMeansResult.clusters,
+			palette: palette
+		};
 	},
 
 	showPalette: function () {
@@ -106,7 +281,7 @@ var origin = {
 		flatData.data = pixels;
 
 		return flatData;
-	}
+	},
 }
 
 // origin image struct
@@ -176,9 +351,13 @@ $(document).ready(function () {
 
 				// get image array
 				origin.pixels = origin.getImageData();
+
 				kmeansResult = origin.getPalette();
+
+
+
 				origin.clusters = kmeansResult.clusters;
-				origin.palette = kmeansResult.centroids;
+				origin.palette = kmeansResult.palette;
 				origin.showPalette();
 
 				hideLoading();
